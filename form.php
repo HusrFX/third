03f5d0b9dd905afb43d3014c95fc79ce519d76d4
HusrFX <hotamovhusrav@gmail.com>
2022-07-24 02:17:48 +0300

    22

--- form.php
+++ form.php
@@ -11,7 +11,7 @@ $product=$products[$_GET['id']];
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="main.css" />
-	<link rel="stylesheet" href="form.css" />
+	<link rel="stylesheet" href="error.css" />
 	<title>Document</title>
 </head>
 <body>
