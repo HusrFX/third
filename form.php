e307469926530c875b3ef7f7de19bca311937831
HusrFX <hotamovhusrav@gmail.com>
2022-07-24 15:56:53 +0300

    sdsd

--- form.php
+++ form.php
@@ -1,8 +1,25 @@
 <?php
-include $_SERVER ['DOCUMENT_ROOT'] . '/products.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/products.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/order.php';
 
-$product=$products[$_GET['id']];
+$product=$products[$_REQUEST['id'] ?? 0];
+$success=false;
+$error=false;
 
+if(! empty($_POST))
+{
+	if(empty($_POST['name']) || empty($_POST['id']))
+	{
+		$error=true;
+	}else{
+		if(makeOrder($product, $_POST['name']))
+		{
+			$success=true;
+		}else {
+			$error=true;
+		}
+	}
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,8 +27,8 @@ $product=$products[$_GET['id']];
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="main.css" />
-	<link rel="stylesheet" href="error.css" />
+	<link rel="stylesheet" href="/main.css" />
+	<link rel="stylesheet" href="/error.css" />
 	<title>Document</title>
 </head>
 <body>
@@ -21,27 +38,32 @@ $product=$products[$_GET['id']];
 	</ul>
 	<div class="main">
 	<h3>Корзина</h3>
-	<p class="success">
+	<?php if($success) {?>
+		<p class="success">
 		Ваша покупка оформлена
-	</p>
-	<p class="error">
+		</p>
+		<?php } else{ ?>
+	<?php if($error) {?>
+		<p class="error">
 		Нужно выбрать товар и заполнить все поля
-	</p>
+		</p>
+	<?php } ?>
 	<div class="items">
 	<figure>
-			<p><img src="<?=$product['img']?>" alt="" /></p>
+			<p><img src="/<?=$product['img']?>" alt="" /></p>
 			<figcaption><?=$product['name']?></figcaption>
 			<figcaption><?=$product['Price']?></figcaption>
 		</figure>
 	</div>
 	
-	<form action="">
+	<form action="/form.php" method="post">
+		<input type="hidden" name="id" value="<?=$product['id']?>">
 		<label>
-			Ваше ФИО:
-			<input type="text" placeholder="Введите ваше ФИО">
+			Ваше ФИО*:
+			<input type="text" name="name" placeholder="Введите ваше ФИО">
 		</label>
 		<input type="submit" value="Купить" class="btn">
-	</form>
+	</form><?php }?>
 	</div>
 </body>
 </html>
--- products.php
+++ products.php
@@ -1,16 +1,19 @@
 <?php
 $products=[
 	[
+		'id' => 0,
 		'name' => 'Ancient',
 		'img' => "img/Ancient.png",
 		'Price' => 'free',
 	],
 	[
+		'id' => 1,
 		'name' => 'Vertigo',
 		'Price' => '$10',
 		'img' => 'img/Vertigo.png',
 	],
 	[
+		'id' => 2,
 		'name' => 'Nuke',
 		'Price' => '$20',
 		'img' => 'img/Nuke.jpg',
